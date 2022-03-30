cd77dae8009baa7184bd713c752e1b859c6164a9
Julien Paul <julien.paul@ymail.com>
2022-03-30 13:38:49 +0200

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     maintainer="BCDC",
     packages=find_packages(include=["django-weathervis"]),
     url="https://github.com/julienpaul/django-weathervis",
-    version="version=0.1.3",
+    version="version=0.1.4",
     zip_safe=False,
 )
